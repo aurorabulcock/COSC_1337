24498c9d775fa4e81d5ef776489945d70939f966
Rory Bulcock <bulcockaurora@gmail.com>
2025-11-06 12:50:57 -0600

    Lab 1 Upload

---
+++ Lab0L1_Hello_World/src/lab0L1/Lab0L1.java
@@ -0,0 +1,19 @@
+/*Aurora "Ro" Bulcock
+ * COSC 1337 001
+ * 8/25/2023
+ * Purpose: To print out "Hello, World!" (Lab0L1)
+ */
+package lab0L1;
+
+public class Lab0L1 {
+
+	public static void main(String[] args) {
+		double a = 4.3, b=2.7;
+		System.out.println("Hello, World!");
+		System.out.println("Hello, World!");
+		System.out.println("The value of a is: "+a+". The value of b is: "+b);
+		//System.out.printf("The value of a is: %d", a);
+	
+	}
+
+}
